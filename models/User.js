c77df267c6bc4d77c08b4fdfad0ffb32716eebbb
Eric Sproles <tron04@gmail.com>
2017-06-21 17:00:56 -0700

    added hearts array object to userSchema

--- models/User.js
+++ models/User.js
@@ -21,8 +21,10 @@ const userSchema = new Schema({
     trim: true
   },
   resetPasswordToken: String,
-  resetPasswordExpires: Date
-
+  resetPasswordExpires: Date,
+  hearts: [
+    { type: mongoose.Schema.ObjectId, ref: 'Store' } // Id's related to a store
+  ]
 });
 
 userSchema.virtual('gravatar').get(function() {
